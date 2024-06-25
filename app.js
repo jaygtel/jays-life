9f1cfb8e55298130cfb9a653ac7615df0b0a41f3
Jay Telford <hello@jaytelford.me>
2024-06-25 15:49:33 +0100

    dropdown nav for games menu

--- app.js
+++ app.js
@@ -15,19 +15,13 @@ const contactRouter = require('./public/routes/contact');
 const aboutRouter = require('./public/routes/about');
 const indexRouter = require('./public/routes/index');
 const blogRouter = require('./public/routes/blog');
-const gamesRouter = require('./public/routes/games');
 const pf2eRouter = require('./public/routes/pf2e');
 const dnd5eRouter = require('./public/routes/dnd5e');
 const sr5Router = require('./public/routes/sr5');
 const applyRouter = require('./public/routes/apply');
 
 // Set up view engine
-const hbs = create({
-  extname: '.hbs',
-  layoutsDir: path.join(__dirname, 'public/views/layouts'),
-  defaultLayout: 'main',
-  helpers: helpers
-});
+const hbs = create({ extname: '.hbs', layoutsDir: path.join(__dirname, 'public/views/layouts'), defaultLayout: 'main', helpers });
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'public/views'));
@@ -42,7 +36,6 @@ app.use('/', indexRouter);
 app.use('/contact', contactRouter);
 app.use('/about', aboutRouter);
 app.use('/blog', blogRouter);
-app.use('/games', gamesRouter);
 app.use('/games/pf2e', pf2eRouter);
 app.use('/games/dnd5e', dnd5eRouter);
 app.use('/games/sr5', sr5Router);
